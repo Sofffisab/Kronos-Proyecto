298af0f8ef4f6446fcc06cf5939a6c7238631278
Mathias <mathifogo@gmail.com>
2025-10-06 14:26:36 -0300

    hecho añadir eventos al calendario bien basico

--- Frontend/src/components/Project Page/Calendar/calendar.jsx
+++ Frontend/src/components/Project Page/Calendar/calendar.jsx
@@ -1,11 +1,32 @@
-import FullCallendar from '@fullcalendar/react'
+import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 import './Calendar.css'
+import CalendarModal from '../../modals/CalendarModal.jsx'
+import { useState } from 'react'
 export default function Calendar(props) {
+    const [date, setDate]= useState()
+    const [tasks, setTasks] = useState([]);
+    const [modal, toggleModal] = useState(false)
+    const addTask = (info)=> {
+        toggleModal(true);
+        setDate(info.date)
+    }
+    const createTask = (title,desc, date)=> {
+        const event = {
+            id: Math.random().toString(),
+            title: title,
+            start: date
+        }
+        setTasks([...tasks, event])
+        toggleModal(false)
+    }
 
     return(
-        <FullCallendar
-        plugins={[ dayGridPlugin ]}
+        <>
+        {modal && <CalendarModal disableModal={()=>toggleModal(false)} submit={(title, desc)=>createTask(title, desc, date)}/>}
+        <FullCalendar
+        plugins={[ dayGridPlugin, interactionPlugin ]}
         initialView="dayGridMonth"
         customButtons={{year:{text: "year"}}}
         headerToolbar={{
@@ -13,6 +34,10 @@ export default function Calendar(props) {
             center: '',
             end: 'prevYear prev today next nextYear' 
           }}
+          selectable={true}
+          dateClick={(info)=>addTask(info)}
+          events={tasks}
        />
+       </>
     )
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,28 +3,18 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import LandingPage from "./Pages/LandingPage.jsx";
 import ErrorPage from "./Pages/ErrorPage.jsx";
-import HomePage from "./Pages/HomePage.jsx";
 import Login from "./Pages/Login.jsx";
 import coconut from '../Public/coconut.jpg'
 import Register from "./Pages/Register.jsx";
-import ProjectSelectPage from "./Pages/ProjectSelectPage.jsx";
 import ProjectPage from "./Pages/ProjectPage.jsx";
-import CalendarPage from "./Pages/CalendarPage.jsx";
-import DashboardPage from "./Pages/DashboardPage.jsx";
-import ProjectConfigPage from "./Pages/ProjectConfigPage.jsx";
 import Recover from "./Pages/Recover.jsx";
 import './global.css'
 const router = createBrowserRouter([
   { path: "/", element: <LandingPage /> },
   { path: "*", element: <ErrorPage /> },
-  { path: "/home", element: <HomePage /> },
   { path: "/login", element: <Login /> },
   { path: "/Register", element: <Register /> },
-  { path: '/projects', element: <ProjectSelectPage/>},
   { path: '/project/:id', element: <ProjectPage/>},
-  { path: '/project/:id/calendar', element: <CalendarPage/>},
-  { path: '/project/:id/dashboard', element: <DashboardPage/>},
-  { path: '/project/:id/config', element: <ProjectConfigPage/>},
   { path: '/recover', element: <Recover /> },
   
 ]);
