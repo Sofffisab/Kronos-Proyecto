9b80e481f8dc4f9146ac102eca458ed3b8d299b5
Mathias <mathifogo@gmail.com>
2025-10-22 14:31:11 -0300

    avances en la pagina de crear proyecto mayte calvert bege

--- Frontend/src/Pages/CreateProjectPage.jsx
+++ Frontend/src/Pages/CreateProjectPage.jsx
@@ -5,22 +5,46 @@ import Navbar from "../components/NavBar.jsx";
 export default function CreateProjectPage() {
 
     const [formData, setFormData] = useState({})
+    const [input, setInput] = useState('')
     const [ step, setStep] = useState(1)
+    const [nombre, setNombre ] = useState('');
+    const [tasks, setTasks ] = useState('');
+    const [type, setType] = useState(null)
     const [ title, setTitle] = useState('¿Que estan preparando actualmente con tu equipo?')
     const nextStep = ()=> {
         loadFormData();
-        setStep(step+1)
-        if(step==2) {setTitle('¿Que tareas necesitas realizar en Plan para reducir '+formData.title)
+        const next = step+1
+        setStep(next)
+        if(next==2) {setTitle('¿Que tareas necesitas realizar en Plan para reducir '+formData.nombre)
         }
 
-        else if(step==3)setTitle('¿Que forma de organización es la mejor para tu proyecto?¡Luego podes cambiarla!')
+        else if(next==3)setTitle('¿Que forma de organización es la mejor para tu proyecto?¡Luego podes cambiarla!')
 
-        else if(step==4) postForm()
+        else if(next==4) postForm()
     }
-    const loadFormrData = ()=> {}
+    const loadFormData = ()=> {
+        if(input.trim() !== ''){ 
+        if(step==1) { 
+            setNombre(input) 
+            setInput('')}
+
+        if(step == 2){
+        
+          if(tasks.length==0)  setTasks[rawTasks]
+        }
+
+             
+        
+
+    }}
+    const rawTasks = []
+    const addTasks = ()=> {
+        rawTasks.push(input);
+        setInput('')
+    }
 
     return( <div className='createPage'>
         <Navbar/>
-        <CreateProjectForm title={title}step={step} nextStep={nextStep}/>
+        <CreateProjectForm addTask={addTasks}input={input}title={title}step={step} nextStep={nextStep} setInput={setInput}/>
         </div>)
 }
--- Frontend/src/components/Create Project/CreateProjectForm.jsx
+++ Frontend/src/components/Create Project/CreateProjectForm.jsx
@@ -7,9 +7,19 @@ export default function CreateProjectForm(props) {
 return(
     <div className={style.form}>
         <p>{props.title}</p>
-    {props.step == 1 && <input placeholder='ej: Plan para reducir consumo de plastico ' onChange={(event)=>props.setTitle(event.target.value)}/>}
+        {(props.step== 1 || props.step== 2) && (
+    <input placeholder= {props.step == 1 ?'ej: Plan para reducir consumo de plastico ' : 'ej: Reunion inicial para hacer brainstorming '}
+     onChange={(event)=>props.setInput(event.target.value)} value={props.input}  onKeyDown={props.step == 2? (e) => e.key === "Enter" && props.addTask() : undefined}/>
+        )}{props.step == 3 && (
+<>
+<SimpleButton class={style.selectBtn} text='Tablero' onClick={()=>props.setType('eq')} icon='equalizer'/>
+<SimpleButton class={style.selectBtn} text='Lista' onClick={()=>props.setType('list')} icon='list'/>
+<SimpleButton class={style.selectBtn} text='Calendario' onClick={()=>props.setType('cal')} icon='calendar_today'/>
+</>
 
+        )}
 
+    
 
 
     <SimpleButton class={style.submitBtn}text='Siguiente' onClick={props.nextStep}/>
